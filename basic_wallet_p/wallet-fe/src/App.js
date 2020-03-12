e69966cf9f7aca5eae15b5602730dbde9a25763a
David Kuseh <davegru203@gmail.com>
2020-03-12 14:36:01 +0000

    create-react-app

---
+++ basic_wallet_p/wallet-fe/src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+        <p>Client Miner</p>
+    </div>
+  );
+}
+
+export default App;
